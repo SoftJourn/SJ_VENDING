3daa4036ab6dace7e27170beb215548d599f88bb
vdanyliuk <vdanyliuk@softjourn.com>
2016-08-05 16:27:10 +0300

    Permit using admin page for all authenticated users.

--- src/main/java/com/softjourn/vending/config/VendingApplication.java
+++ src/main/java/com/softjourn/vending/config/VendingApplication.java
@@ -82,7 +82,7 @@ public class VendingApplication extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/admin").authenticated()
                 .anyRequest().permitAll()
             .and()
             .sessionManagement()

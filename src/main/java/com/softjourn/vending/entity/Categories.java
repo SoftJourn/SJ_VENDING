e0e7e28c848118f1b716e975a2588aa1b89bf210
vkraietskyi <vkraietskyi@softjourn.com>
2016-10-07 11:43:49 +0300

    Updated sql init script

--- src/main/java/com/softjourn/vending/entity/Categories.java
+++ src/main/java/com/softjourn/vending/entity/Categories.java
@@ -21,7 +21,7 @@ import javax.validation.constraints.Pattern;
 public class Categories {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true)

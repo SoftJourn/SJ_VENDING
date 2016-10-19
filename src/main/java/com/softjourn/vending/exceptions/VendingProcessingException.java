3776088a85e80cbb592b3ade922fa07f31c8786e
vdanyliuk <vdanyliuk@softjourn.com>
2016-10-19 12:02:17 +0300

    Buying in real machine

---
+++ src/main/java/com/softjourn/vending/exceptions/VendingProcessingException.java
@@ -0,0 +1,9 @@
+package com.softjourn.vending.exceptions;
+
+public class VendingProcessingException extends RuntimeException {
+
+    public VendingProcessingException(String message) {
+        super(message);
+    }
+
+}

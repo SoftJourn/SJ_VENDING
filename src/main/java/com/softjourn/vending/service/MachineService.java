ecc5b778bef5f7ee3934e1363860659cddb845f6
vdanyliuk <vdanyliuk@softjourn.com>
2016-10-25 15:48:02 +0300

    Signed requests to machine

--- src/main/java/com/softjourn/vending/service/MachineService.java
+++ src/main/java/com/softjourn/vending/service/MachineService.java
@@ -1,23 +1,41 @@
 package com.softjourn.vending.service;
 
 
-import com.softjourn.vending.dto.TransactionDTO;
 import com.softjourn.vending.entity.VendingMachine;
 import com.softjourn.vending.exceptions.VendingProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
+import java.math.BigInteger;
+import java.security.*;
+import java.time.Instant;
+import java.util.Collections;
 import java.util.Optional;
 
 @Service
 public class MachineService {
 
+    private static final String AUTH_HEADER_NAME = "Authorization";
+
+    @Value("${machine.request.signer.keystore.file}")
+    String machineSignerKeystoreFile;
+    @Value("${machine.request.signer.keystore.password}")
+    String keystorePassword;
+    @Value("${machine.request.signer.keystore.alias}")
+    String keystoreAlias;
+
     private VendingService vendingService;
     private RestTemplate template;
+    private Signature signature;
 
     public MachineService(VendingService vendingService, RestTemplate template) {
         this.vendingService = vendingService;
@@ -45,11 +63,40 @@ public class MachineService {
     }
 
     private int post(String url, String fieldInternalId) {
-        ResponseEntity<TransactionDTO> response =  template.exchange(url,
-                HttpMethod.POST,
-                new HttpEntity<>(fieldInternalId),
-                TransactionDTO.class);
-        return response.getStatusCode().value();
+            HttpHeaders authHeader = new HttpHeaders();
+            authHeader.put(AUTH_HEADER_NAME, Collections.singletonList(createSignedData()));
+
+            ResponseEntity<String> response = template.exchange(url,
+                    HttpMethod.POST,
+                    new HttpEntity<>(fieldInternalId, authHeader),
+                    String.class);
+            System.out.println(response.getBody());
+            return response.getStatusCode().value();
+    }
+
+    private String createSignedData() {
+        String raw = Instant.now().toEpochMilli() + "";
+        try {
+            signature.update(raw.getBytes());
+            String signed = new BigInteger(signature.sign()).toString(16);
+            return raw + "." + signed;
+        } catch (SignatureException e) {
+            throw new RuntimeException("Can't sign data " + raw, e);
+        }
+    }
+
+    @PostConstruct
+    private void prepareSignerKey() {
+        try {
+            KeyPair keyPair = new KeyStoreKeyFactory(
+                    new ClassPathResource(machineSignerKeystoreFile), keystorePassword.toCharArray())
+                    .getKeyPair(keystoreAlias, keystorePassword.toCharArray());
+
+            signature = Signature.getInstance("SHA256withRSA");
+            signature.initSign(keyPair.getPrivate());
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException("Can't prepare machine request signer. ", e);
+        }
     }
 
 

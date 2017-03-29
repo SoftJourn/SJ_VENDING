cfd929814f1cd5b6d4328ff85a4d22a84d60deea
vdanyliuk <vdanyliuk@softjourn.com>
2017-03-29 16:48:58 +0300

    Do not update timestamp on updating other fields

--- src/main/db/schema.update.18.sql
+++ src/main/db/schema.update.18.sql
@@ -1,6 +1,9 @@
 ALTER TABLE purchases ADD COLUMN product_name VARCHAR(255) NOT NULL;
 ALTER TABLE purchases ADD COLUMN product_price DECIMAL(19, 2) NOT NULL;
 
+ALTER TABLE purchases MODIFY COLUMN time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE products MODIFY COLUMN added_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
 UPDATE purchases SET product_name = (SELECT products.name FROM products WHERE products.id = purchases.product);
 UPDATE purchases SET product_price = (SELECT products.price FROM products WHERE products.id = purchases.product);
 
